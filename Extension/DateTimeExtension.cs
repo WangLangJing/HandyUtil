20c819bdf50a5d12e15af6de2458e42b344eef97
WangLangJing <932444208@qq.com>
2017-09-17 17:53:57 +0800

    添加项目文件。

---
+++ Extension/DateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HandyUtil
+{
+    public static class  DateTimeExtension
+    {
+        /// <summary>
+        /// 表示当前日期是否是周六或者周日
+        /// </summary>
+        public static Boolean IsHoliday(this DateTime dateTime)
+        {
+            Int32 weekDay = (Int32)dateTime.DayOfWeek;
+            return (weekDay == 6 || weekDay == 0);
+        }
+    }
+}
---
+++ Extension/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HandyUtil
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 判断字符串是空引用或者是空字符串
+        /// </summary>
+        public static Boolean IsNullOrEmpty(this String str)
+        {
+            return String.IsNullOrEmpty(str);
+        }
+        /// <summary>
+        /// 判断字符串是空引用，或者完全是由空白字符组成的
+        /// </summary>
+        public static Boolean IsNullOrWhiteSpace(this String str)
+        {
+            return String.IsNullOrWhiteSpace(str);
+        }
+    }
+}
